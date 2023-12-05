ae5c7ca9a08a580870993721a17c3d7c7698cbce
Michał Kopczewski <kopczewski.michal101@gmail.com>
2023-12-05 13:47:08 +0100

    lab7 zad2 śledzenie myszy działa i nie rzuca błędami

--- Front/lab7/src/App.js
+++ Front/lab7/src/App.js
@@ -8,21 +8,21 @@ import './style.css';
 export default function App() {
   const TrackMouse = () => {
     const handleMouseMove = event => {
-      setMouseChange({
-        x: event.clientX,
-        y: event.clientY
-    })
+      setMouseChange([
+        event.clientX,
+        event.clientY
+      ])
     } 
 
 
-    const [mousePosition, setMouseChange] = useState({x: 0, y:0})
+    const [mousePosition, setMouseChange] = useState([0,0])
     useEffect(() => {
       window.addEventListener('mousemove', handleMouseMove);
     }, mousePosition);    
     
   return (
     <div>
-      <p>Pozycja kursora myszy: ({mousePosition.x},{mousePosition.y})</p>
+      <p>Pozycja kursora myszy: ({mousePosition[0]},{mousePosition[1]})</p>
     </div>
   )  
   }
