94392a089ecff6aeeeaf68943c6cc0823f645034
Michał Kopczewski <kopczewski.michal101@gmail.com>
2023-12-06 18:57:19 +0100

    Działa zad 7 lab4

--- Front/lab7/src/App.js
+++ Front/lab7/src/App.js
@@ -1,123 +1,99 @@
 // zadanie 4 lab7
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  
-  const [products, setProducts] = useState([])
-  const DisplayList = () => {
-    function getShoppingList() {
-      axios.get('https://fakestoreapi.com/products')
-      .then((response) => {
-        setProducts(response.data)})
-      .catch(error => {
-        console.error('Error fetching shopping list:', error);
-      });
-    }
-    
-    
-    useEffect(() => {
-      getShoppingList()
+  const [products, setProducts] = useState([]);
 
-    })
-    
-    return (
-      <ul>
-        {products.map((elem, index) => (
-          <li key={index}>{elem.title} price: {elem.price}</li>
-        ))}
-      </ul>
-    )
+  function getShoppingList() {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching shopping list:", error);
+      });
   }
 
-  
-  const Form = () => {
-    const formProperties = [
-      {name: "title", type: "text"},
-      {name: "price", type: "text"},
-      {name: "description", type: "text"},
-      {name: "image", type: "text"},
-      {name: "category", type: "text"}
-    ]
-    // const [formData, setFormData] = useState(['' ,'', '','',''])
-    const [formData, setFormData] = useState(['Komputer','40.23', 'Opis','obrazek','coś innego'])
-    
-    useEffect(()=>{
-      
-    },[formData])
-    
-    const handleFormDataChange = (event, index) => {
-      let newFormData = [...formData]
-      newFormData[index] = event.target.value
-      setFormData(newFormData)
+  useEffect(() => {
+    getShoppingList();
+  }, []);
+
+  const formProperties = [
+    { name: "title", type: "text" },
+    { name: "price", type: "text" },
+    { name: "description", type: "text" },
+    { name: "image", type: "text" },
+    { name: "category", type: "text" },
+  ];
+  const [formData, setFormData] = useState(['' ,'', '','',''])
+  // const [formData, setFormData] = useState([
+  //   "Komputer",
+  //   "40.23",
+  //   "Opis",
+  //   "obrazek",
+  //   "coś innego",
+  // ]);
+
+  useEffect(() => {}, [formData]);
+
+  const handleFormDataChange = (event, index) => {
+    let newFormData = [...formData];
+    newFormData[index] = event.target.value;
+    setFormData(newFormData);
+  };
+
+  const handleConfirm = (event) => {
+    event.preventDefault();
+    const newProduct = {
+      title: formData[0],
+      price: formData[1],
+      description: formData[2],
+      image: formData[3],
+      category: formData[4],
     }
-    
-    // app.post("/games/:id", (req, res) => {
-    //   const id = req.params.id;
-    //   const moveData = req.body; // pozycja spacja symbol
-    //   const position = parseInt(moveData[0]);
-    //   const symbol = moveData[2]
-    //   if (games[id][position - 1] === " ") {
-    //     games[id][position - 1] = symbol
-    //     res.send(`${gameToBoardString(games[id])}\n`)
-    //   } else {
-    //     res.send(`Nie możesz w tym miejscu wykonać ruchu, nie jest ono puste\n${gameToBoardString(games[id])}\n`)
-    //   }
-    // {
-
-    const handleConfirm = event => {
-      event.preventDefault();
-      const newProduct = JSON.stringify(
-        {
-            title: formData[0],
-            price: formData[1],
-            description: formData[2],
-            image: formData[3],
-            category: formData[4]
-        })
-      fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body: newProduct
-        })
-            .then(res => {
-              if (res.status === 200) {
-                setProducts([...products, newProduct])
-                console.log("powinno działać")  
-              }
-            })
+    const newProductJSON = JSON.stringify(newProduct);
+    fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      body: newProductJSON,
+    }).then((res) => {
+      if (res.status === 200) {
+        console.log(newProduct);
+        setProducts([...products, newProduct]);
       }
+    });
+  };
 
-    return (
+  return (
+    <div>
+      <p>Pobieramy wszystkie dane produktów, ale wyświetlanie opisu i linku do obrazka sprawia że czytelność jest zerowa</p>
+      <ul>
+        {products.map((elem, index) => (
+          <li key={index}>
+            {elem.title} price: {elem.price} category:{elem.category} 
+          </li>
+        ))}
+      </ul>
+
       <form>
         {formProperties.map((elem, index) => (
           <p key={index}>
             <label>{elem.name}</label>
-            <input type={elem.type}
-            key={index}
-            id={index}
-            value={formData[index]}
-            onChange={(event) => handleFormDataChange(event, index)}
+            <input
+              type={elem.type}
+              key={index}
+              id={index}
+              value={formData[index]}
+              onChange={(event) => handleFormDataChange(event, index)}
             ></input>
           </p>
-            ))}
-          <p>
-            <button
-            onClick={handleConfirm}
-            >
-              Submit
-            </button>
-          </p>
+        ))}
+        <p>
+          <button onClick={handleConfirm}>Submit</button>
+        </p>
       </form>
-    )
-  }
-
-
-
-
-  return (
-  <div>
-      <DisplayList/>
-      <Form/>
-  </div>
-   );
+    </div>
+  );
 }
